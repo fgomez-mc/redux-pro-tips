8925c2b6efed3013be9e652b06257931453460d9
KF Gomez <fgomez@mangochango.com>
2021-01-09 15:03:10 -0600

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
-import { render } from "react-dom";
 import renderer from "react-test-renderer";
+import * as redux from "react-redux";
 import App from "./App";
 
 describe("App", () => {
-  test("renders learn react link", () => {
-    const testRenderer = renderer.create(<App />);
+  jest.spyOn(redux, "useDispatch");
+  const useSelectorMock = jest.spyOn(redux, "useSelector");
+  const albums = Array(6)
+    .fill(null)
+    .map((_, i) => ({ id: i, title: `Album #${i}` }));
+
+  test("renders learn react link", async () => {
+    useSelectorMock.mockReturnValueOnce(albums);
+    const testRenderer = await renderer.create(<App />);
     expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
